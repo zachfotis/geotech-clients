d68095b4ce5317a4baf277f5de30cdfe01e9899c
Fotios N. Zachopoulos <zaxfotis@gmail.com>
2022-07-04 16:06:10 +0300

    add fetch to aade

--- src/components/dashboard/CreateCompany.jsx
+++ src/components/dashboard/CreateCompany.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { toast } from 'react-toastify';
 import SpinnerSmall from '../layout/SpinnerSmall';
 
 function CreateCompany() {
@@ -6,67 +7,75 @@ function CreateCompany() {
   const [isFetching, setIsFetching] = useState(false);
   const [vat, setVat] = useState('');
   const [createCompanyForm, setCreateCompanyForm] = useState({
-    country: '',
-    companyName: '',
-    firstname: '',
-    lastname: '',
-    businessType: '',
-    telephone: '',
-    addressName: '',
-    addressNumber: '',
+    doy: '',
+    name: '',
+    title: '',
+    legal: '',
+    address: '',
+    number: '',
+    zip: '',
     city: '',
-    zipCode: '',
-    email: '',
+    registration: '',
+    businessType: [],
   });
 
   useEffect(() => {
-    const fetchVat = async () => {
+    const fetchVatFromGeotechServer = async () => {
       setIsFetching(true);
+      try {
+        const response = await fetch(`https://geotech-server.herokuapp.com/api/v1/getVat/${vat}`);
+        if (response.status !== 200) {
+          throw new Error('Something went wrong');
+        }
+        const data = await response.json();
+        setCreateCompanyForm({
+          ...createCompanyForm,
+          ...data,
+        });
+      } catch (error) {
+        toast.error('VAT number is not valid');
+        setCreateCompanyForm({
+          doy: '',
+          name: '',
+          title: '',
+          legal: '',
+          address: '',
+          number: '',
+          zip: '',
+          city: '',
+          registration: '',
+          businessType: [],
+        });
+      }
 
-      const body = `<env:Envelope xmlns:env="http://www.w3.org/2003/05/soap-envelope" xmlns:ns1="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd" xmlns:ns2="http://rgwspublic2/RgWsPublic2Service" xmlns:ns3="http://rgwspublic2/RgWsPublic2">
-      <env:Header>
-          <ns1:Security>
-              <ns1:UsernameToken>
-                  <ns1:Username>GEOTECH-09</ns1:Username>
-                  <ns1:Password>geotech!09</ns1:Password>
-              </ns1:UsernameToken>
-          </ns1:Security>
-      </env:Header>
-      <env:Body>
-          <ns2:rgWsPublic2AfmMethod>
-              <ns2:INPUT_REC>
-                  <ns3:afm_called_by/>
-                  <ns3:afm_called_for>081827757</ns3:afm_called_for>
-              </ns2:INPUT_REC>
-          </ns2:rgWsPublic2AfmMethod>
-      </env:Body>
-      </env:Envelope>`;
-
-      const url = 'https://www1.gsis.gr/wsaade/RgWsPublic2/RgWsPublic2';
-      const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/soap+xml;charset=utf-8',
-          'Content-Length': body.length,
-        },
-        body,
-      });
-
-      const data = await response.text();
-      const xml = new DOMParser().parseFromString(data, 'text/xml');
-      console.log(xml);
       setIsFetching(false);
     };
 
-    if (vat.length === 9) {
-      fetchVat();
+    if (!isEditing && vat.length === 9) {
+      fetchVatFromGeotechServer();
     }
   }, [vat]);
+
+  const onReset = () => {
+    setVat('');
+    setCreateCompanyForm({
+      doy: '',
+      name: '',
+      title: '',
+      legal: '',
+      address: '',
+      number: '',
+      zip: '',
+      city: '',
+      registration: '',
+      businessType: [],
+    });
+  };
 
   return (
     <div className="create-company">
       <h1 className="text-xl font-bold">{!isEditing ? 'Create New' : 'Modify Existing'} Company</h1>
-      <form className="create-company-form">
+      <form className="create-company-form" onReset={onReset}>
         <div className="checkbox-container">
           <label htmlFor="editCheckbox" className="label-text">
             Edit Mode
@@ -91,26 +100,58 @@ function CreateCompany() {
           />
           {isFetching && <SpinnerSmall />}
         </div>
-        <input type="text" required={true} placeholder="Company Name" className="input input-bordered input-ghost" />
+        <input
+          type="text"
+          required={true}
+          placeholder="Company Name"
+          className="input input-bordered input-ghost"
+          value={createCompanyForm.name}
+          onChange={(e) => setCreateCompanyForm({ ...createCompanyForm, name: e.target.value })}
+        />
         <input
           type="text"
           required={true}
           placeholder="Type of Business"
           className="input input-bordered input-ghost"
+          value={createCompanyForm.businessType}
+          onChange={(e) => setCreateCompanyForm({ ...createCompanyForm, businessType: e.target.value })}
         />
         <input type="text" required={true} placeholder="Phone Number" className="input input-bordered input-ghost" />
         <div className="address-container">
-          <input type="text" required={true} placeholder="Address Name" className="input input-bordered input-ghost" />
+          <input
+            type="text"
+            required={true}
+            placeholder="Address Name"
+            className="input input-bordered input-ghost"
+            value={createCompanyForm.address}
+            onChange={(e) => setCreateCompanyForm({ ...createCompanyForm, address: e.target.value })}
+          />
           <input
             type="text"
             required={true}
             placeholder="Address Number"
             className="input input-bordered input-ghost"
+            value={createCompanyForm.number}
+            onChange={(e) => setCreateCompanyForm({ ...createCompanyForm, number: e.target.value })}
           />
         </div>
         <div className="address-container-2">
-          <input type="text" required={true} placeholder="City" className="input input-bordered input-ghost" />
-          <input type="text" required={true} placeholder="Postal Code" className="input input-bordered input-ghost" />
+          <input
+            type="text"
+            required={true}
+            placeholder="City"
+            className="input input-bordered input-ghost"
+            value={createCompanyForm.city}
+            onChange={(e) => setCreateCompanyForm({ ...createCompanyForm, city: e.target.value })}
+          />
+          <input
+            type="text"
+            required={true}
+            placeholder="Postal Code"
+            className="input input-bordered input-ghost"
+            value={createCompanyForm.zip}
+            onChange={(e) => setCreateCompanyForm({ ...createCompanyForm, zip: e.target.value })}
+          />
         </div>
         <input type="text" required={true} placeholder="Country" className="input input-bordered input-ghost" />
         <input type="email" required={true} placeholder="Email" className="input input-bordered input-ghost" />
