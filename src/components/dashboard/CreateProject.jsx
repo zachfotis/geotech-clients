09eebcbe6b8813aa30dee4c7791e23419d03e7db
Fotios N. Zachopoulos <zaxfotis@gmail.com>
2022-07-25 13:50:17 +0300

    fix project futures and invalid date inputs

--- src/components/dashboard/CreateProject.jsx
+++ src/components/dashboard/CreateProject.jsx
@@ -185,9 +185,20 @@ function CreateProject() {
             className="input input-bordered input-ghost"
             value={new Date(projectForm.timestamp).toISOString().split('T')[0]}
             onChange={(e) => {
-              const date = e.target.value;
-              const timestamp = new Date(date).getTime();
-              setProjectForm({ ...projectForm, timestamp });
+              try {
+                const date = e.target.value;
+                const timestamp = new Date(date).getTime();
+                // Check for future date
+                if (timestamp > new Date().getTime()) {
+                  throw new Error('Future dates are not allowed');
+                }
+                // Checker checks the input is a valid date
+                const checker = new Date(timestamp).toISOString().split('T')[0];
+                setProjectForm({ ...projectForm, timestamp });
+              } catch (error) {
+                toast.error(error.message);
+                setProjectForm({ ...projectForm, timestamp: new Date().getTime() });
+              }
             }}
           />
         </div>
